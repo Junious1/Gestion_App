368df8c23424686d9e8e002363d1709dc4dca11a
Junious1 <juniortraoretkmj225@gmail.com>
2023-08-10 19:14:02 +0200

    commit du 10/08/2023

--- lib/pages/modif_compte_client.dart
+++ lib/pages/modif_compte_client.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
-
 import '../components/Config.dart';
 import '../components/NavBar.dart';
 import '../components/flutter_flow_widgets.dart';
